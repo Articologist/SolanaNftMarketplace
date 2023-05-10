4260cf24c4b8ff10d70d4eab7e5c17fe3d95679d
HA <105129021+Articologist@users.noreply.github.com>
2023-05-09 18:28:19 -0600

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -35,7 +35,7 @@ export const HomeView: FC = ({ }) => {
         <div className="md:hero mx-auto p-4">
           <div className="md:hero-content flex flex-col">
             <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-              Welcome to Solana{" "}
+              Welcome to Audio Documents{" "}
               <span>
                 <Typewriter
                   options={{
