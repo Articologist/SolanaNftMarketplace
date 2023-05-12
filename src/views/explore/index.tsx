c341d68833b6dfcf47d8e3143d596ad0dd332360
HA <105129021+Articologist@users.noreply.github.com>
2023-05-12 07:37:54 -0600

    Update index.tsx

--- src/views/explore/index.tsx
+++ src/views/explore/index.tsx
@@ -8,7 +8,7 @@ export const ExploreView: FC = ({ }) => {
        
         <div className="md:hero mx-auto p-4">
           <div className="md:hero-content flex flex-col">
-            <h1 className="text-center text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
+            <h1 className="text-center text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#000000] to-[#336699]">
               Explore audi0 Nfts
             </h1>
             
