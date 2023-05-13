5f32532ccfc7ab9d76a72c988d830e323743daca
HA <105129021+Articologist@users.noreply.github.com>
2023-05-13 13:59:20 -0600

    Update FetchRariableNfts.tsx

--- src/components/FetchRariableNfts.tsx
+++ src/components/FetchRariableNfts.tsx
@@ -53,18 +53,18 @@ export const FetchRariableNfts: FC = () => {
                                         <div className={styles.nftinfo}>
                                             <div className={styles.nftinfoname}> {nft?.blockchain}  </div>
                                           {/*  <div className={styles.nftinfoname}> {nft?.url}  </div> */}
-                                            <div className={styles.nftinfoname}> {nft?.meta?.externalUri}  </div>
+                                            <div className={styles.nftinfoname}> {nft?.meta?.external_url}  </div>
 
                                         </div>
                                     </div>
 
                                 </div>
-                                {/* <button
+                                <button
                                             className="px-8 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
-                                        // onClick={buy}
+                                        // onClick={external_url}
                                         >
                                             Buy
-                                        </button> */}
+                                        </button>
                                 {/* --Buy Button java_onclick */}
                                  {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
                                     <div className="flex justify-center ">
