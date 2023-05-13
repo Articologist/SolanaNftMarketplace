d5b93c70729e64dda1b76fd3894ba9215ba5f8c0
HA <105129021+Articologist@users.noreply.github.com>
2023-05-13 14:37:04 -0600

    Update FetchRariableNfts.tsx

--- src/components/FetchRariableNfts.tsx
+++ src/components/FetchRariableNfts.tsx
@@ -58,11 +58,11 @@ export const FetchRariableNfts: FC = () => {
                                     </div>
 
                                 </div>
-                                <button
+                               {/* <button
                                             className="px-8 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
                                             // onClick={?}
                                             Buy
-                                        </button>
+                                        </button>  */}
                                 {/* --Buy Button java_onclick */}
                                  {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
                                     <div className="flex justify-center ">
