daeeb5a4ba5cbce78f1ad236a24d11dbe847a18b
HA <105129021+Articologist@users.noreply.github.com>
2023-05-13 16:55:35 -0600

    Update FetchRariableNfts.tsx

--- src/components/FetchRariableNfts.tsx
+++ src/components/FetchRariableNfts.tsx
@@ -68,7 +68,7 @@ export const FetchRariableNfts: FC = () => {
                                  <button className="px-8 btn btn-sm btn-outline-success animate-pulse bg-gradient-to-r from-[#ffffff] to-[#ffffff] {styles.nftinfoname}"
                                             onClick={nft?.content?.url}>Buy</button>
                                  
-                                        // onClick={() => {styles.nftinfoname}>{nft?.content?.url}</button>
+                                        {/*  onClick={() => {styles.nftinfoname}>{nft?.content?.url}</button> */}
                                    
 
 
