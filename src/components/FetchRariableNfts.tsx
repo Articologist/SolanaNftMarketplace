e749d020eebb6582b4035dea25bc6d991c5778ac
HA <105129021+Articologist@users.noreply.github.com>
2023-05-13 15:18:32 -0600

    Update FetchRariableNfts.tsx

--- src/components/FetchRariableNfts.tsx
+++ src/components/FetchRariableNfts.tsx
@@ -57,10 +57,10 @@ export const FetchRariableNfts: FC = () => {
                                     </div>
 
                                 </div>
-                                                                          <div className={styles.nftinfoname}> {nft?.bestSellOrder?.status}  </div>
+                                                                          <div className={styles.nftinfoname}> {nft?.content?.available}  </div>
                                 <div></div>
                                <button
-                                            className="px-8 btn btn-outline-success animate-pulse bg-gradient-to-r from-[#ffffff] to-[#ffffff]">
+                                            className="px-8 btn-sm btn-outline-success animate-pulse bg-gradient-to-r from-[#ffffff] to-[#ffffff]">
                                             <a href="https://chat.blockscan.com/index?a=0xFA5b602B211D4CB638254920fD6A79A5C8A7C7Cb">
                                             Chat </a>
                                         </button>
