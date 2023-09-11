427c6e03007a67b9902f129c4bb5153af187f701
HA <105129021+Articologist@users.noreply.github.com>
2023-09-10 21:22:25 -0600

    Update index.tsx

--- src/views/explore/index.tsx
+++ src/views/explore/index.tsx
@@ -11,7 +11,14 @@ export const ExploreView: FC = ({ }) => {
             <h1 className="text-center text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#000000] to-[#336699]">
             {/*  Explore audi0 Nfts */}
             </h1>
-            
+            <iframe
+  id="inlineFrameExample"
+  title="Inline Frame Example"
+  width="100%"
+  height="1200"
+  src="https://audocs.notion.site/Getting-Started-with-Audio-Wallet-25e5407503f44d5ebe2f5c7d56ec9cca?pvs=4"
+>
+</iframe>
             {/* CONTENT GOES HERE */}
             <div className="text-center">
             {/*  <FetchRariableNfts /> */}
